8139b8ee15fbe31157e9167fd2bcb8971cfbd58e
Vanessa Huhn <35490743+vanessahuhn@users.noreply.github.com>
2018-07-18 10:00:23 +0200

    Add files via upload

---
+++ Tableaux/src/Exercice1.java
@@ -0,0 +1,15 @@
+
+public class Exercice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] tab = {'a', 'v', 'i', 'o', 'n'};
+		
+		for (int pos=0; pos<5; pos++) {
+			if (tab[pos] == 'i') {
+					System.out.println("YES");
+			}
+		}
+	}
+
+}
